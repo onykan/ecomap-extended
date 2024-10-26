e50f89bc0d5945ad8807445b227bfb54a94f2902
otvinyka <otto.v.nykanen@student.jyu.fi>
2024-10-26 14:11:27 +0300

    testing some data storage stuff

---
+++ server/controllers/datastore.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const maplayers = path.join(__dirname, "./data/maplayers.json");
+
+function initMapLayers(layerQueryMap) {
+    layerQueryMap.forEach((query, layer) => {
+        
+    });
+}
