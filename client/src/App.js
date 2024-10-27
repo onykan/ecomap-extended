bcd14c3db77a65d78d733ac2bdfe9b4b9fac41e6
Iljinisa <iljinisa@student.jyu.fi>
2024-10-27 16:33:56 +0200

    gdp change beetween two submitted years to be colorcoded on the map

--- client/src/App.js
+++ client/src/App.js
@@ -19,15 +19,86 @@ function App () {
 
 export default App;
 */
-import React from 'react'
-import Map from './components/Map'
+import React, { useState, useRef } from 'react';
+import Map from './components/Map';
 
 function App() {
+  const [dateBeg, setDateBeg] = useState('2022');
+  const [dateEnd, setDateEnd] = useState('2023');
+  const dateBegRef = useRef(null);
+  const dateEndRef = useRef(null);
+
+  /// Handles the form submit
+  /// sets values to dateBeg and dateEnd
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (checkValues()) {
+      return;
+    }
+    const newDateBeg = dateBegRef.current.value;
+    const newDateEnd = dateEndRef.current.value;
+    setDateBeg(newDateBeg);
+    setDateEnd(newDateEnd);
+    console.log({ dateBeg: newDateBeg, dateEnd: newDateEnd });
+
+    // You can add any validation or processing here if needed
+  };
+
+
+  /// Checks ways to validate the input
+  /// The input must be a number and the year range is from 1960 to 2023
+  /// also the end year must be greater than the begin year
+  /// Maximum range is 30 years
+  function checkValues() {
+    const newDateBeg = dateBegRef.current.value;
+    const newDateEnd = dateEndRef.current.value;
+    if (newDateBeg === '' || newDateEnd === '') {
+      alert('Please enter a value for both fields');
+      return true;
+    }
+    if (isNaN(newDateBeg) || isNaN(newDateEnd)) {
+      alert('Please enter a valid number');
+      return true;
+    }
+    if (newDateBeg < 1960 || newDateEnd > 2023 || newDateBeg > newDateEnd) {
+      alert('Please enter a valid year. The year range is from 1960 to 2023');
+      return true;
+    }
+    if (newDateEnd - newDateBeg > 30) {
+      alert('The maximum range is 30 years');
+      return true;
+    }
+    return false;
+  }
+
   return (
     <div>
-      <Map />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Begin year:
+          <input
+            type="text"
+            name="datebeg"
+            ref={dateBegRef}
+            defaultValue={dateBeg}
+          />
+        </label>
+        <label>
+          End year:
+          <input
+            type="text"
+            name="dateEnd"
+            ref={dateEndRef}
+            defaultValue={dateEnd}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <div>
+        <Map dateBeg={dateBeg} dateEnd={dateEnd} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,32 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import axios from 'axios';
+import { scaleBand, scaleLinear, scalePow } from "d3-scale";
 
+
+// Changed the map to this version to get ISO3 codes for the countries
 const geoUrl =
-  "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+  "https://raw.githubusercontent.com/lotusms/world-map-data/main/world.json";
 
-const Map = () => {
+const Map = ({dateBeg, dateEnd}) => {
   const [data, setData] = useState([]);
-/*
+  /// Fetch data from api and parses it to the format that is used in the map
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('http://localhost:3001/api/countries');
-      setData(result.data);
-      console.log(result.data)
+      const result = await axios.get('api/indicator/gdp?dateBeg='+dateBeg+'&dateEnd='+dateEnd+'');
+      parsedata(result.data);
     };
     fetchData();
-  }, []);
-  */
+  }, [dateBeg, dateEnd]);
+  
+  /// Parses the data so the gdp is a persentual change from the first year to the last year
+  /// Sets the data to the state
+  /// If first or last year is null, the gdp is set to null
+  //If given years have only one data point, the gdp is set to null
+  const parsedata = (data) => {
+      const newData = {};
+      Object.keys(data).forEach(countryCode => {
+        const countryData = data[countryCode];
+        
+        const gdpLast = countryData[0].value;
+        const gdpFirst = countryData[countryData.length-1].value;
 
+        if (gdpFirst != null && gdpLast != null && countryData.length > 1) {
+          const gdpChangePersentage = ((gdpLast / gdpFirst)-1) * 100;
+          newData[countryCode] = {gdp: gdpChangePersentage, countryiso3code: countryCode};
+        }
+        else {
+          newData[countryCode] = {gdp: null, countryiso3code: countryCode};
+        }
+      });
+      setData(newData);
+  };
+    
+  /// First checks which geography is which country with iso3 code
+  /// uses d3 linear color scale to get a color for the country based on the gdp persentage
+  /// Right now the scale is from -3 to 3 based on the normal yearly gpd change
   const getCountryColor = (countryName) => {
-    const countryData = data.find((country) => country.country === countryName);
-    if (!countryData) return "#EEE"
-    console.log(countryData)
-    const { gdp } = countryData;
+    const countryData = Object.values(data).flat().find((entry) => entry.countryiso3code === countryName);
+    if (!countryData) return "#EEE";
 
-    if (gdp > 10000) return "#4CAF50" //green
-    if (gdp > 1000) return "#FFC107" //yellow
-    return "#F44336" //red
+    const colorScale = scaleLinear().domain([-3,3]).range(["red", "green"]);
+    const { gdp: gdp } = countryData;
+    return colorScale(gdp);
+
   }
 
   return (
@@ -34,16 +60,15 @@ const Map = () => {
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
           geographies.map((geo) => {
-        
-            const countryName = geo.properties.name; // by name TEMPORARILY
+            const countryName = geo.id;
             return (
               <Geography
                 key={geo.rsmKey}
                 geography={geo}
-                //fill={getCountryColor(countryName)}
+                fill={getCountryColor(countryName)}
                 style={{
                   default: { outline: "none" },
-                  hover: { fill: "#2B6CB0", outline: "none" },
+                  hover: { fill: "#2B6CB0", outline: "none", cursor: "pointer"},
                   pressed: { outline: "none" },
                 }}
               />
@@ -51,8 +76,9 @@ const Map = () => {
           })
         }
       </Geographies>
+      
     </ComposableMap>
   );
 };
 
-export default Map
+export default Map;
