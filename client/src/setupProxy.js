56d8335963eae9352cc70aeb6d71e4d3cced8dfe
lepton <tonihirvonen00@gmail.com>
2024-11-07 06:12:20 +0200

    proxy fix fix
    proxy route from '/' to '/api'

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// TODO: not loading the env vars
+// require('dotenv').config({ path: '../../.env' });
+const PROXY = process.env.REACT_APP_PROXY || 'localhost:3001';
+const targetServer = `http://${PROXY}/api`;
+
 module.exports = function(app) {
+  console.log(`Proxy on ${targetServer}`)
   app.use(
-    '/',
+    '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: targetServer,
       changeOrigin: true,
     })
   );
