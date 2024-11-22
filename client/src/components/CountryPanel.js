19bcb6821b1ae751b56ab1a4b58674fb16ad385e
Niko Iljin <iljinisa@student.jyu.fi>
2024-11-22 13:26:37 +0200

    a base of chart added for the panel component

--- client/src/components/CountryPanel.js
+++ client/src/components/CountryPanel.js
@@ -1,6 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
+import { CategoryScale, Chart as ChartJS, LinearScale, LineElement, PointElement, Title, Tooltip, Legend, plugins } from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const CountryPanel = ({ country, isOpen, onClose }) => {
+  const [countryData, setCountryData] = useState([]);
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    if (country && isOpen) {
+      axios.get(`/api/country/${country.code}/data`)
+        .then((response) => {
+          const data = response.data[country.code].info;
+          setCountryData(data);
+
+          const cData = response.data[country.code].indicators
+
+          console.log("data", cData);
+          const labels = Object.keys(cData.GDP);
+          console.log("labels", labels);
+          const datasets = Object.keys(cData).map((key) => {
+            return {
+              label: key,
+              data: Object.values(cData[key]),
+              borderColor: 'rgba(75,192,192,1)',
+            }
+          }
+          );
+          console.log("datasets", datasets);
+
+          setChartData({
+            labels: labels,
+            datasets: datasets,
+          })
+        }
+        
+      );
+    }
+  }
+  , [country, isOpen]);
+
+  const options = {
+
+      responsive : true,
+      plugins: {
+      legend: {position: "top"},
+      title: {
+      display: true,
+      text: 'GDP of Country',
+      }
+  }}
+
   return (
     <div
       style={{
@@ -8,7 +68,7 @@ const CountryPanel = ({ country, isOpen, onClose }) => {
         right: 0,
         top: 0,
         height: "100vh",
-        width: isOpen ? "300px" : "0px",
+        width: isOpen ? "600px" : "0px",
         overflow: "hidden",
         backgroundColor: "#f4f4f4",
         boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
@@ -16,11 +76,12 @@ const CountryPanel = ({ country, isOpen, onClose }) => {
         padding: isOpen ? "20px" : "0px",
       }}
     >
-      {isOpen && country && (
+      {isOpen && chartData && (
         <div>
           <button onClick={onClose} style={{ float: "right" }}>Close</button>
           <h2>Country: {country.code}</h2>
           <p>Indicator Value: {country.value}</p>
+          <Line data={chartData} options={options}/>
         </div>
       )}
     </div>
